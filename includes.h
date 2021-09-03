ac90dce3637dddec63224c5a4c47fc7cf930bdf3
Wladek <wldzam1981@gmail.com>
2021-09-03 19:50:40 +0300

    minsk

---
+++ includes.h
@@ -0,0 +1,35 @@
+//#pragma once
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+// libs
+#include <math.h>
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+//setlocale(LC_ALL, "Rus");
+// qt
+//#include <QCoreApplication>
+//#include <QDebug>
+//#include <QGuiApplication>
+//#include <QObject>
+//#include <QProcess>
+//#include <QQmlApplicationEngine>
+//#include <QQmlContext>
+//#include <QSqlQuery>
+//#include <QtSql/QSqlDatabase>
+//#include <QtSql/QSqlDriver>
+//#include <QtSql/QSqlDriverCreatorBase>
+//#include <QtSql/QSqlDriverPlugin>
+//#include <QtSql/QSqlError>
+
+using namespace std;
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+using namespace std;
+
+int main()
+{
+
+    int i;
+    cin >> i;
+    if (i > 10) {
+        cout << "ok" << endl;
+    } else {
+        cout << "bad" << endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //this_thread::sleep_for(chrono::seconds(1));
+
+    return 0;
+}
